aa447c425a7e53fb52079c2e3e7ac53428b6af64
Fuji_Toan <nguyenvantoantha.hua@gmail.com>
2018-04-23 08:50:10 +0700

    Change branch webpage

--- app/Http/Controllers/MigrationController.php
+++ app/Http/Controllers/MigrationController.php
@@ -6,13 +6,17 @@ use Illuminate\Http\Request;
 
 class MigrationController extends Controller
 {
-    public function index($action)
+    public function index($action, Request $request)
     {
+        //Get git branches
+        $result = shell_exec("git branch");
+        $branches = array_filter(explode("\n",$result));
+
         //Migration controller
         $output = "";
         switch ($action) {
             case 'home':
-                return view('migration', ['output' => 'Welcome Migration page']);
+                return view('migration', ['output' => 'Welcome Migration page','branches' => $branches]);
                 break;
             case 'code':
                 $output = shell_exec("git pull origin master");
@@ -25,20 +29,26 @@ class MigrationController extends Controller
                 $output .= shell_exec("/usr/local/bin/composer dump-autoload");
                 $output .= shell_exec("php ../artisan db:seed");
                 break;
+            case 'branch':
+                $output = shell_exec('git checkout '.$request->branch);
+                //Update branch
+                $result = shell_exec("git branch");
+                $branches = array_filter(explode("\n",$result));
+                break;
             default:
-                return view('migration', ['output' => 'Welcome Migration page']);
+                return view('migration', ['output' => 'Welcome Migration page','branches' => $branches]);
                 break;
         }
 
         //Send message to Chatwork
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"https://api.chatwork.com/v2/rooms/{room-id}/messages");
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,"body=".urlencode($output);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-ChatWorkToken: {Access-token}'));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_exec ($ch);
-        curl_close ($ch);
-        return view('migration', ['output' => $output]);
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL,"https://api.chatwork.com/v2/rooms/{room-id}/messages");
+        // curl_setopt($ch, CURLOPT_POST, 1);
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, 'body='.urlencode($output);
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-ChatWorkToken: {Access-token}'));
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // curl_exec ($ch);
+        // curl_close ($ch);
+        return view('migration', ['output' => $output,'branches' => $branches]);
     }
 }
--- resources/views/migration.blade.php
+++ resources/views/migration.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Migration page</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
         <div class="container">
@@ -11,9 +11,25 @@
                   <pre>{{ $output }}</pre>
             </div>
             <div class="row">
-                <div class="col-sm-4" style="text-align: center;"><a title="To pull latest source codes on Server " href="/migration/code" class="btn btn-primary">Migrate Code</a></div>
-                <div class="col-sm-4" style="text-align: center;"><a title="To drop all tables from the database and then execute the migrate command" href="/migration/database-fresh" class="btn btn-info">Migrate Database Fresh</a></div>
-                <div class="col-sm-4" style="text-align: center;"><a title="To run all of your outstanding migrations, execute the migrate Artisan command" href="/migration/database-migrate" class="btn btn-info">Migrate Database</a></div>
+            <form action="/migration/branch">
+                <div class="form-group">
+                    <label>Branch:</label>
+                    <select name="branch">
+                        @foreach ($branches as $branch)
+                            @if ($branch[0]=='*')
+                                <option value="{{trim(substr($branch,1))}}" selected>{{trim(substr($branch,1))}}</option>
+                            @else
+                                <option value="{{trim($branch)}}">{{trim($branch)}}</option>
+                            @endif
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3" style="text-align: center;"><button class="btn btn-warning">Change Git branch</button></div>
+                <div class="col-sm-3" style="text-align: center;"><a title="To pull latest source codes on Server " href="/migration/code" class="btn btn-primary">Migrate Code</a></div>
+                <div class="col-sm-3" style="text-align: center;"><a title="To drop all tables from the database and then execute the migrate command" href="/migration/database-fresh" class="btn btn-info">Migrate Database Fresh</a></div>
+                <div class="col-sm-3" style="text-align: center;"><a title="To run all of your outstanding migrations, execute the migrate Artisan command" href="/migration/database-migrate" class="btn btn-info">Migrate Database</a></div>
             </div>
         </div>
     </body>
